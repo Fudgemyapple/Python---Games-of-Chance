28ed8ec7ffda4c6328d14313ff7e730486c25138
Fudgemyapple <65399270+Fudgemyapple@users.noreply.github.com>
2020-05-25 12:20:34 +0100

    user inputs added to heads or tails game

--- Games_of_Chance.py
+++ Games_of_Chance.py
@@ -14,9 +14,22 @@ def coin_flip_bet(bet, outcome, money):
     if outcome == zero_or_one():
         winnings = bet*2
         money = money + winnings 
-        return print("You have WON! Your winnings for this game are " + str(winnings) + "!") 
+        return print("You have WON! Your winnings for this game are $" + str(winnings) + "!") 
     else:
         losses = bet
-        return print("You have LOST! Your losses for this game are -" + str(losses) + "!") 
-    
-coin_flip_bet(100, 1, money)
+        return print("You have LOST! Your losses for this game are -$" + str(losses) + "!") 
+
+def heads_or_tails_conversion(heads_or_tails):
+    if heads_or_tails == "Heads":
+        outcome_bet = 0
+        return outcome_bet
+    else:
+        heads_or_tails == "Tails"
+        outcome_bet = 1
+        return outcome_bet
+
+print("Welcome to the heads or tails game first you need to play a bet below. You have up to $100 to play with.")
+bet_amount = input("Amount you would like to bet $")
+heads_or_tails = input("Heads or Tails?")
+outcome_bet = heads_or_tails_conversion(heads_or_tails)
+coin_flip_bet(int(bet_amount), outcome_bet, money)
