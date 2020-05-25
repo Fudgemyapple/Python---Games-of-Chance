3e135c0f69857287d009f2f48c681960440ffd14
Fudgemyapple <65399270+Fudgemyapple@users.noreply.github.com>
2020-05-25 13:50:41 +0100

    debugging

--- Games_of_Chance.py
+++ Games_of_Chance.py
@@ -1,7 +1,6 @@
-import random
+import random 
 
 money = 100
-
 
 #Coint Flip Game
 
@@ -11,16 +10,17 @@ def zero_or_one():
 
 def coin_flip_bet(bet, outcome, money):
     money = money - bet
-    if outcome == zero_or_one():
+    flip = zero_or_one()
+    if outcome == flip:
         winnings = bet*2
         money = money + winnings 
-        if zero_or_one == 1:
+        if flip == 1:
             result = "Tails"
         else:
             result = "Heads" 
         return print("It was " + result + "!" + " You have WON! Your winnings for this game are $" + str(winnings) + "!") 
     else:
-        if zero_or_one == 1:
+        if flip == 1:
             result = "Tails"
         else:
             result = "Heads"
@@ -28,15 +28,28 @@ def coin_flip_bet(bet, outcome, money):
         return print("It was " + result + "!" + " You have LOST! Your losses for this game are -$" + str(losses) + "!") 
 
 def heads_or_tails_conversion(heads_or_tails):
-    if heads_or_tails == "Heads":
+    if heads_or_tails.lower() == "heads":
         outcome_bet = 0
         return outcome_bet
-    elif heads_or_tails == "Tails":
+    elif heads_or_tails.lower() == "tails":
         outcome_bet = 1
         return outcome_bet
 
+def check_money(bet_amount, money):
+    if bet_amount <= money:
+        return 
+    else:
+        print("You don't have enough money to bet that amount") 
+        return 
+
 print("Welcome to the heads or tails game first you need to play a bet below. You have up to $100 to play with.")
 bet_amount = input("Amount you would like to bet $")
+while int(bet_amount) > money:
+    print("You don't have enough money to bet that amount. You have $" + str(money)) 
+    bet_amount = input("How much would you like to bet?" " $")
 heads_or_tails = input("Heads or Tails?")
+while heads_or_tails.lower != "heads" or heads_or_tails.lower != "tails":
+    print("You have not entered either heads or tails.")
+    heads_or_tails = input("Heads or Tails?")
 outcome_bet = heads_or_tails_conversion(heads_or_tails)
 coin_flip_bet(int(bet_amount), outcome_bet, money)
