80f04531693cbab190590e97731e6c6dbe2ff2b6
Fudgemyapple <65399270+Fudgemyapple@users.noreply.github.com>
2020-05-25 12:01:05 +0100

    Coin flip game created

--- Games_of_Chance.py
+++ Games_of_Chance.py
@@ -9,4 +9,14 @@ def zero_or_one():
     ans = random.randint(0, 1)
     return ans
 
-print(zero_or_one())
+def coin_flip_bet(bet, outcome, money):
+    money = money - bet
+    if outcome == zero_or_one():
+        winnings = bet*2
+        money = money + winnings 
+        return print("You have WON! Your winnings for this game are " + str(winnings) + "!") 
+    else:
+        losses = bet
+        return print("You have LOST! Your losses for this game are -" + str(losses) + "!") 
+    
+coin_flip_bet(100, 1, money)
